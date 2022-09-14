5e5d77823244a3210437476e0e422501b83f5cf7
dmitryspeliy <dmitriypospelov93@gmail.com>
2022-09-14 14:02:58 +0700

    HW2

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 package ru.skyPro;
 
 import ru.skyPro.faculties.Gryffindor;
+import ru.skyPro.faculties.Hufflepuff;
+import ru.skyPro.faculties.Ravenclaw;
 import ru.skyPro.faculties.Slytherin;
 
 public class Main {
@@ -14,9 +16,15 @@ public class Main {
         Slytherin malfoy = new Slytherin("Дарко малфой",31,30,
                 40,60,100,100, 100);
 
+        Hufflepuff smith = new Hufflepuff(" Захария Смит",41,21,30,60,90);
+
+        Ravenclaw chang = new Ravenclaw("Чжоу Чанг",41,10,80,100,12,92);
+
         System.out.println(malfoy.showStudent());
         System.out.println(harry.compareStudentOfGriff(germiona));
         System.out.println(malfoy.compareStudents(harry));
+        System.out.println(smith.showStudent());
+        System.out.println(chang.compareStudents(smith));
 
 
     }
